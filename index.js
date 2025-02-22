d35b1bb2698613fa15936fb666e9266582e3fafe
Anand-Yadav-create <anandprakashhacker@gmail.com>
2025-02-22 09:02:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const corsoptions={
-    origin:'https://engineers-hub-1.onrender.com',
+    origin:'https://frontendpro-e3ja.onrender.com.onrender.com',
     credentials:true,
     
    
